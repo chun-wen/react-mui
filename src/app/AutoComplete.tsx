d15da724ec59f2d6af4c657178c37064ef801174
andy.yu <andy@3drens.com>
2023-11-26 22:12:32 +0800

    feat: remove default style and add AutoComplete component

---
+++ src/app/AutoComplete.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import Paper from '@mui/material/Paper';
+import Stack from '@mui/material/Stack';
+import { styled } from '@mui/material/styles';
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
+
+const Test = () => {
+  return (
+    <Stack direction='column' spacing={2}>
+      <Item>Item 1</Item>
+      <Item>Item 1</Item>
+      <Item>Item 1</Item>
+      <Item>Item 1</Item>
+    </Stack>
+  );
+};
+
+export default Test;
